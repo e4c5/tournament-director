9cf1f71acd5c7136e7638edf56c88c4596fd78ad
Raditha Dissanayake <raditha.dissanayake@gmail.com>
2017-05-14 06:19:27 +0530

    A new serializer to write results

--- pairing/serializers.py
+++ pairing/serializers.py
@@ -1,21 +1,39 @@
+from django.db.models import Q
 from pairing.models import Player, Result , Standing
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
 
-class PlayerSerializer(ModelSerializer):
+class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = '__all__'
         read_only_fields = ('id',)
         
 
-class ResultSerializer(ModelSerializer):
+class ResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = Result
         fields = '__all__'
         read_only_fields = ('id',)
         depth = 1
         
-class StandingSerializer(ModelSerializer):
+class ResultUpdateSerializer(serializers.Serializer):
+    player1 = serializers.CharField()
+    player2 = serializers.CharField()
+    score1 = serializers.IntegerField()
+    score2 = serializers.IntegerField()
+    game = serializers.IntegerField()
+    
+    def validate(self, data):
+        if Result.objects.filter( Q(game=data['game']) 
+                                & Q(player1__name=data['player1']) 
+                                & Q(player2__name=data['player2'])).count() != 1:
+            raise ValidationError("Invalid pairing")
+    
+        return data
+    
+    
+class StandingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Standing
         fields = '__all__'
--- pairing/views.py
+++ pairing/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 
 from pairing.models import Player, Result, Standing
 from pairing.serializers import PlayerSerializer, ResultSerializer,\
-    StandingSerializer 
+    StandingSerializer, ResultUpdateSerializer
+     
 from pairing import utils
 from rest_framework.decorators import list_route
 
@@ -16,15 +18,34 @@ class PlayerViewSet(viewsets.ModelViewSet):
         player = self.queryset.select_related('results').get(pk)
         return Response(self.serializer_class(player))
     
+
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
     
     def list(self, request):
+        '''
+        Returns the available results for the latest round
+        '''
         rnd = utils.get_current_round()
-        serializer = self.serializer_class(self.queryset.filter(game=rnd),many=True)
+        qs = self.queryset.filter(game=rnd).exclude(score1=None)
+        serializer = self.serializer_class(qs,many=True)
         
         return Response(serializer.data)
+    
+    def create(self, request):
+        '''
+        Note that we use a different serializer here
+        '''
+        s = ResultUpdateSerializer(data=request.data)
+        if s.is_valid():
+            Result.objects.filter( Q(player1__name=s.validated_data['player1']) & 
+                                   Q(player2__name=s.validated_data['player2']) &
+                                   Q(game=s.validated_data['game'])
+                                   ).update(score1=s.validated_data['score1'], score2=s.validated_data['score2'])
+            return Response({'status': 'OK'})
+        else:
+            return Response({'error': 'Players mismatched'}, status=status.HTTP_412_PRECONDITION_FAILED)
     
 class RoundViewSet(viewsets.ViewSet):
     '''
